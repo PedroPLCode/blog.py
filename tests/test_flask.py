1e6789820f2d1e11bb8d63456e3eb4abb642d212
Piotr Gaszczynski <piotrek.gaszczynski@gmail.com>
2024-03-31 11:47:25 +0200

    Add unit tests.

--- tests/test_flask.py
+++ tests/test_flask.py
@@ -1,46 +1,73 @@
-from blog import app
-from unittest.mock import Mock
 import pytest
+from flask import url_for
+from blog import app, db
+from blog.models import Entry
 
-"""
-@pytest.mark.parametrize('path, arg', (
-    ('/', 'movie/popular'),
-    ('/?list_type=WRONG_LIST_TYPE', 'movie/popular'),
-    ('/?WRONG_PARAM=WRONG_LIST_TYPE', 'movie/popular'),
-    ('/?list_type=top_rated', 'movie/top_rated'),
-    ('/?list_type=upcoming', 'movie/upcoming'),
-    ('/?list_type=now_playing', 'movie/now_playing'),
-    ('/search?q=TEST_QUERY', 'search/movie?query=TEST_QUERY'),
-    ('/today?timezone=TEST_QUERY', 'tv/airing_today?timezone=TEST_QUERY'),
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        with app.app_context():
+            yield client
+            
+            
+@pytest.mark.parametrize(('redirects', 'code', 'location'), (
+    (True, 200, None),
+    (False, 302, '/'),
 ))
-"""
+def test_wrong_path_return_codes_and_redirects_location(client, redirects, code, location):
+    response = client.get('/WRONG_PATH', follow_redirects=redirects)
+    assert response.status_code == code
+    assert response.location == location
+            
+            
+@pytest.mark.parametrize('path', (
+    ('/drafts/'),
+    ('/new-post/'),
+    ('/edit-post/1'),
+))
+def test_login_required_redirects_to_login_when_method_GET(client, path):
+    response = client.get(path)
+    assert response.status_code == 302
+    assert response.location.endswith(url_for('login', next=path))
 
-#Czy @login_required przekierowuje do widoku login, przekazując mu parametr next?
-def test_login_required_redirects_to_view_with_next_param():
-    pass
 
-#Czy widok dodawania, modyfikacji i usuwania wpisu wymaga logowania?
-def test_login_required_to_create_new_entry():
-    pass
+def test_login_required_redirects_to_login_when_method_POST(client):
+    response = client.post('/delete-post/1', data={'homepage': 'false'})
+    assert response.status_code == 302
+    assert response.location.endswith(url_for('login', next='/delete-post/1'))
 
-def test_login_required_to_edit_entry():
-    pass
 
-def test_login_required_to_delete_entry():
-    pass
-
-#Czy widok szkiców wymaga logowania?
-def test_login_required_to_get_list_drafts():
-    pass
-
-#Czy widok szkiców pokazuje tylko szkice?
-def test_only_not_published_in_list_drafts():
-    pass
-
-#Czy na stronie głównej znajdują się jedynie opublikowane materiały?
-def test_only_published_in_homepage_view():
-    pass
-
-#Czy aplikacja zwraca błąd 404, jeśli odwołamy się do nieistniejącego id posta, w widoku edycji?    
-def test_error_404_if_edit_not_existing_post():
-    pass
+def test_homepage_shows_only_published_entries_and_drafts_shows_only_drafts(client):
+    draft_test_entry = Entry(title="Draft Test 1", 
+                             content="Draft content test 1", 
+                             is_published=False,
+                             )
+    published_test_entry = Entry(title="Published Test 1", 
+                                 content="Published content test 1", 
+                                 is_published=True,
+                                 )
+    db.session.add(draft_test_entry)
+    db.session.add(published_test_entry)
+    db.session.commit()
+    
+    with client.session_transaction() as session:
+        session['logged_in'] = True
+        
+    response_drafts = client.get('/drafts/', follow_redirects=True)
+    assert response_drafts.status_code == 200
+    assert b"Draft Test 1" in response_drafts.data
+    assert b"Draft content test 1" in response_drafts.data
+    assert b"Published Test 1" not in response_drafts.data
+    assert b"Published content test 1" not in response_drafts.data
+    
+    response_posts = client.get('/')
+    assert response_posts.status_code == 200
+    assert b"Draft Test 1" not in response_posts.data
+    assert b"Draft content test 1" not in response_posts.data
+    assert b"Published Test 1" in response_posts.data
+    assert b"Published content test 1" in response_posts.data
+    
+    db.session.delete(draft_test_entry)
+    db.session.delete(published_test_entry)
+    db.session.commit()
